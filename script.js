484a2729b0f18a4b7ba7a1dfd93d48bfe1e3c7bf
unknown <ramonlegendario21@gmail.com>
2023-01-06 13:51:31 -0300

    marker cluster

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 
+
+
 let h2 = document.querySelector('h2');
 var map;
 
 function success(pos){
     console.log(pos.coords.latitude, pos.coords.longitude);
-    h2.textContent = `Latitude: ${pos.coords.latitude}, Longitude: ${pos.coords.longitude}`;
+    h2.textContent = `Informações de Vazamento em Salvador`;
+    //h2.textContent = `Latitude: ${pos.coords.latitude}, Longitude: ${pos.coords.longitude}`;
+
+
 
     //Condição para mudar de posição sem dar erro
     if(map === undefined){
-        map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 15);
+        map = L.map('map').setView([-12.9777125, -38.4713629], 15);
 
     }else{
         map.remove();
-        map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 15);
+        map = L.map('map').setView([-12.9777125, -38.4713629], 15);
 
     }
 
@@ -22,10 +27,88 @@ function success(pos){
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-    //Marcador do mapa
-    var marker = L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map);
-    var popup = marker.bindPopup('<b>Localização</b><br />Voce está aqui.');
-    popup.openPopup();
+
+//geojeson data
+
+
+
+    const geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+    };
+
+    var dados = [{
+        "type": "Feature",
+        "properties": {
+            "name": "Coors Field",
+            "amenity": "Baseball Stadium",
+            "popupContent": "This is where the Rockies play!"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-38.456569, -12.978306 ],
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Coors Field",
+            "amenity": "Baseball Stadium",
+            "popupContent": "This is where the Rockies play!"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-38.4544, -12.981777 ],
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Coors Field",
+            "amenity": "Baseball Stadium",
+            "popupContent": "This is where the Rockies play!"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-38.454432, -12.979646 ], 
+
+
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Coors Field",
+            "amenity": "Baseball Stadium",
+            "popupContent": "This is where the Rockies play!"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-38.451488, -12.979090 ], 
+
+
+        }
+    },
+];
+
+ 
+ 
+    //const markers = L.markerClusterGroup();
+
+    L.geoJSON(dados, {
+        pointToLayer: function (feature, latlng) {
+           // return  markers.addLayer(L.circleMarker(latlng, geojsonMarkerOptions)) 
+          return L.circleMarker(latlng, geojsonMarkerOptions)
+          }
+
+    }).addTo(map);
+
+       // map.addLayer(markers);
+    
 
 }
 
